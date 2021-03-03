a83c0ddbf118e1136ef6e4d5f65365e5bf094639
TheMightyMaddie <54825775+TheMightyMaddie@users.noreply.github.com>
2021-03-02 22:43:58 -0700

    - Added photos to Graphic Design & UI/UX Design pages.

--- web/js/index.js
+++ web/js/index.js
@@ -1,6 +1,6 @@
-AOS.init({
-	duration: 1200,
-});
+// AOS.init({
+// 	duration: 1200,
+// });
 
 function renderView(element) {
 	$('.page').each(function() {
@@ -18,7 +18,27 @@ function bindHandlers () {
 		})
 		$(this).addClass('selectedNav');
 		renderView(this);
+		bindHandlers();
+	});
+
+	AOS.init({
+		duration: 1200,
 	});
 }
 bindHandlers();
+
+
+function rippleEffect(event) {
+	let targetBoundingRect = event.currentTarget.getBoundingClientRect();
+	let x = event.clientX - targetBoundingRect.x;
+	let y = event.clientY - targetBoundingRect.y;
+	let ripples = document.createElement('span');
+	ripples.style.left = `${x}px`;
+	ripples.style.top = `${y}px`;
+	ripples.classList.add('ripple');
+	event.currentTarget.appendChild(ripples);
+	setTimeout(() => {
+		ripples.remove();
+	}, 600);
+}
 
